5913a43e69870176db43d00608f66040434629e0
howard <howard_bb@hotmail.com>
2021-11-30 19:54:34 +0800

    new : setup default data

---
+++ src/utils/Setup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author howar
+ */
+public class Setup {
+    public static void main(String[] args) {
+        Settings.createDefaultSettings();
+        DataFaker.initAllData();
+    }
+}
