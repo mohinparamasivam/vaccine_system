fed1d02c994f1247b3a20d988d2df1388cbb1bcd
howard <howard_bb@hotmail.com>
2021-11-30 19:53:54 +0800

    new: tests

---
+++ test/DAO/CentreDAOTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package DAO;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author howar
+ */
+public class CentreDAOTest {
+
+    public CentreDAOTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
---
+++ test/utils/SettingsTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package utils;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author howar
+ */
+public class SettingsTest {
+
+    public SettingsTest() {
+    }
+
+    /**
+     * Test of createDefaultSettings method, of class Settings.
+     */
+    @Test
+    public void testCreateDefaultSettings() {
+        Settings.createDefaultSettings();
+    }
+
+}
